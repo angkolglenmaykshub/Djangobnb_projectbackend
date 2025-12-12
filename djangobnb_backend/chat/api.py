b0b7e2029518c54498e9232419cbe95bec854ff2
Glene Miko <glenemiko.jaudian@msunaawan.edu.ph>
2025-12-12 22:40:18 +0800

    Video 8 Fix the refresh tokens- Glene Miko Jaudian

--- djangobnb_backend/chat/api.py
+++ djangobnb_backend/chat/api.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 
 
 from .models import Conversation, ConversationMessage
-from .serializers import ConversationListSerializer, ConversationDetailSerializer
+from .serializers import ConversationListSerializer, ConversationDetailSerializer, ConversationMessageSerializer
 
 
 from useraccount.models import User
@@ -29,32 +29,29 @@ def conversations_detail(request, pk):
 
 
     conversation_serializer = ConversationDetailSerializer(conversation, many=False)
-    # messages_serializer = ConversationMessageSerializer(conversation.messages.all(), many=True)
+    messages_serializer = ConversationMessageSerializer(conversation.messages.all(), many=True)
 
 
     return JsonResponse({
         'conversation': conversation_serializer.data,
-        # 'messages': messages_serializer.data
+        'messages': messages_serializer.data
     }, safe=False)
-
-
 
 
 @api_view(['GET'])
 def conversations_start(request, user_id):
-    try:
-        other_user = User.objects.get(pk=user_id)
-    except User.DoesNotExist:
-        return JsonResponse({'error': 'User not found'}, status=404)
+    conversations = Conversation.objects.filter(users__in=[user_id]).filter(users__in=[request.user.id])
 
 
-    conversation = Conversation.objects.filter(users=request.user).filter(users=other_user).first()
-
-
-    if not conversation:
+    if conversations.count() > 0:
+        conversation = conversations.first()
+       
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
+    else:
+        user = User.objects.get(pk=user_id)
         conversation = Conversation.objects.create()
-        conversation.users.add(request.user, other_user)
+        conversation.users.add(request.user)
+        conversation.users.add(user)
 
 
-    serializer = ConversationDetailSerializer(conversation)
-    return JsonResponse(serializer.data)
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
--- djangobnb_backend/chat/serializers.py
+++ djangobnb_backend/chat/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 
+
 from .models import Conversation, ConversationMessage
 
+
 from useraccount.serializers import UserDetailSerializer
+
+
 
 
 class ConversationListSerializer(serializers.ModelSerializer):
@@ -14,6 +18,8 @@ class ConversationListSerializer(serializers.ModelSerializer):
         fields = ('id', 'users', 'modified_at',)
 
 
+
+
 class ConversationDetailSerializer(serializers.ModelSerializer):
     users = UserDetailSerializer(many=True, read_only=True)
 
@@ -21,3 +27,15 @@ class ConversationDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ('id', 'users', 'modified_at',)
+
+
+
+
+class ConversationMessageSerializer(serializers.ModelSerializer):
+    sent_to = UserDetailSerializer(many=False, read_only=True)
+    created_by = UserDetailSerializer(many=False, read_only=True)
+
+
+    class Meta:
+        model = ConversationMessage
+        fields = ('id', 'body', 'sent_to', 'created_by',)
